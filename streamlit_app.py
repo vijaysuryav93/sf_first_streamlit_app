0c83252497783e399abdc8b013f949bc20c0bd16
Vijay Surya V <vijaysuryav93@gmail.com>
2023-03-29 11:54:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 
-streamlit.title("First Streamlit App by VJ !!!")
-streamlit.header("Streamlit header")
-streamlit.text("streamlit text")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
