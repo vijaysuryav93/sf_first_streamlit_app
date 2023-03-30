53add882a78e98aa68eb8d7adf7583b7e67fca96
Vijay Surya V <vijaysuryav93@gmail.com>
2023-03-30 06:34:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,5 +52,5 @@ streamlit.text(my_data_row)
 
 my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("The Fruit Load Contains")
-streamlit.text(my_data_row)
+streamlit.header("The Fruit Load Contains")
+streamlit.dataframe(my_data_row)
