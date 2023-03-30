d9ab2c87ae8c00af6ca016b43bb7c01331119e6c
Vijay Surya V <vijaysuryav93@gmail.com>
2023-03-30 06:40:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,3 +59,5 @@ streamlit.dataframe(my_data_row)
 #Method 3
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values('form streamlit)")
